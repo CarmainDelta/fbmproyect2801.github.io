1e251d2ad06de591f20b03a4273a8a02555c6e63
CarmainDelta <fernando000@gmail.com>
2021-11-10 16:36:07 -0600

    Guardado de Autentifdicacion

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDuCGPspl3bvDB0-NCjdc_WQ8IHEeHdNCc",
+  authDomain: "fbmproyect1801.firebaseapp.com",
+  projectId: "fbmproyect1801",
+  storageBucket: "fbmproyect1801.appspot.com",
+  messagingSenderId: "540250655754",
+  appId: "1:540250655754:web:19113120ef1b0e147163f3",
+  measurementId: "G-Y83M99YJEM"
+    });   
